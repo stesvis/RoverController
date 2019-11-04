020e5d87bd22dd0061f7aedc1381bdb880da5639
stesvis <stesvis@gmail.com>
2019-11-04 15:42:48 -0700

    Tweaked ListView in iOS

---
+++ RoverController.Mobile/RoverController.Mobile.iOS/Renderers/CustomListViewRenderer.cs
@@ -0,0 +1,21 @@
+﻿using RoverController.Mobile.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ListView), typeof(CustomListViewRenderer))]
+
+namespace RoverController.Mobile.iOS.Renderers
+{
+    public class CustomListViewRenderer : ListViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (this.Control == null) return;
+
+            this.Control.TableFooterView = new UIView();
+        }
+    }
+}
