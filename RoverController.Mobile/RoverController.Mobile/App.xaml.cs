b9354d210f8fddbf05daa259d87756188d31eccf
stesvis <stesvis@gmail.com>
2019-11-04 12:46:00 -0700

    Removed the DebugPage

--- RoverController.Mobile/RoverController.Mobile/App.xaml.cs
+++ RoverController.Mobile/RoverController.Mobile/App.xaml.cs
@@ -55,7 +55,6 @@ namespace RoverController.Mobile
             containerRegistry.RegisterForNavigation<SettingsPage, SettingsPageViewModel>("Settings");
             containerRegistry.RegisterForNavigation<NewMissionPage, NewMissionPageViewModel>("NewMission");
             containerRegistry.RegisterForNavigation<MissionDetailsPage, MissionDetailsPageViewModel>("MissionDetails");
-            containerRegistry.RegisterForNavigation<DebugPage, DebugPageViewModel>();
         }
     }
 }
